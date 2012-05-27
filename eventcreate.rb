41abe059b570ca892827d768110cabd2a26aeb48
Tony Pelletier <tony.pelletier@gmail.com>
2012-05-27 15:05:20 -0400

    Added logging.

--- eventcreate.rb
+++ eventcreate.rb
@@ -1,8 +1,25 @@
 require 'savon'
 require 'yaml'
+require 'logger'
 
+LOGGER = Logger.new('output.log', 'w')
+LOGGER.level = Logger::DEBUG
 
-CONFIG = YAML.load_file("config.yml")
+begin
+  CONFIG = YAML.load_file("config.yml")
+rescue Exception => e
+  puts "No such file or directory - config.yml  Exiting."
+  LOGGER.debug { "No such file or directory - config.yml  Exiting." }
+  Process.exit
+end
+
+if CONFIG["companyName"] == "COMPANYNAME"
+  puts "=" * 100
+  puts "Please edit the config.yml file and change COMPANYNAME to your company name."
+  puts "=" * 100
+  Process.exit
+end
+
 
 
 Gyoku.convert_symbols_to :camelcase
@@ -13,17 +30,22 @@ class Service
   attr_reader :header
 
   def initialize
-    #Dev URL
-    @client = Savon::Client.new("https://developer4.envoyww.com/WebService/EPAPI_1.0/wsdl.wsdl")
-    @client.wsdl.soap_actions
-    @header = { :AuthHeader => {
-        :Domain => "xxx",
-        :UserId => "xxx",
-        :UserPassword => "xxx",
-        :OemId => "xxx",
-        :OemPassword => "xxx"
+    begin
+      @client = Savon::Client.new("https://developer4.envoyww.com/WebService/EPAPI_1.0/wsdl.wsdl")
+      @client.wsdl.soap_actions
+      @header = { :AuthHeader => {
+          :Domain => "x",
+          :UserId => "x",
+          :UserPassword => "x",
+          :OemId => "x",
+          :OemPassword => "x"
+        }
       }
-    }
+    rescue Savon::HTTP::Error => fault
+      LOGGER.debug { fault }
+    rescue Savon::SOAP::Fault => fault
+      LOGGER.debug { fault }
+    end
 
     begin
       service = client.request :organization_query_root do
@@ -32,22 +54,18 @@ class Service
       end
       @orgid = service.to_hash[:organization_query_root_response][:organization][:organization_id]
     rescue Savon::SOAP::Fault => fault
-      fault.to_s
+      result = fault.to_s
+      puts result
+      if result.include?('Credential Check Failed')
+        LOGGER.debug { "Credential Check Failed!"}
+      else
+        LOGGER.debug { fault }
+        fault.to_s
+      end
     end
   end
 end
 
-
-def query_members_by_org(svc)
-  begin
-    service = svc.client.request :member_query_by_organization_id do
-      soap.header = svc.header
-      soap.body = {:organization_ids => svc.orgid, :index => 1, :length => 100}
-    end
-  rescue Savon::SOAP::Fault => fault
-    fault.to_s
-  end
-end
 
 
 def create_event(svc)
@@ -65,6 +83,7 @@ def create_event(svc)
         }
       }
     end
+    LOGGER.debug { service.to_xml }
   rescue Savon::SOAP::Fault => fault
     fault.to_s
 
@@ -75,6 +94,7 @@ end
 if __FILE__ == $0
   svc = Service.new
   create_event(svc)
+  LOGGER.close
 end
 
 
